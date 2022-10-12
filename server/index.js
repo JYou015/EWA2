3eeacc29631ee75c6df647a86854280d0e68bf3c
JYou015 <jason.you015@gmail.com>
2022-10-12 13:23:59 -0400

    jdlkfjsdlkf

---
+++ server/index.js
@@ -0,0 +1,16 @@
+// server/index.js
+const express = require("express");
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.get("/api", (req, res) => {
+    res.json({ message: "Hello from Express!" });
+  });
+
+  app.get("/items", (req, res) => {
+    res.json({ message: "Hello from Express!" });
+  });
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
