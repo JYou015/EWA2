68e31694f0787f14d60a5bf26509611837d73ac9
JYou015 <jason.you015@gmail.com>
2022-10-14 21:06:53 -0400

    aaa

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const path = require("path");
 const app = express();
 app.use(express.json());
 
-app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.use(express.static(path.resolve(__dirname, "client/build")));
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
