d96847ba8f411d6b3453c4a7f3e970879425ea2f
JYou015 <jason.you015@gmail.com>
2022-10-14 21:23:37 -0400

    aa

--- server/index.js
+++ server/index.js
@@ -5,10 +5,10 @@ const path = require("path");
 const app = express();
 app.use(express.json());
 
-app.use(express.static(path.resolve(path.join(__dirname, './client/build'))));
+app.use(express.static(path.join(path.join(__dirname, './client/build'))));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './client/build/index.html'));
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
 
 app.post('/calc', (req, res) => {
